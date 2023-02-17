3f9499714795fb9167a8d10113dfffec62219432
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-02-17 15:18:01 +0100

    Added combined decon-recylce script
    A new combined version, all in one

---
+++ 0. Stand Alone/Farming/Decon & Recycle- 0.5 x 0.5.lua
@@ -0,0 +1,41 @@
+<size=150%><sprite name="Engineering_Bar2"></size>
+<line-height=-17px>
+</line-height><align=right>
+<size=-3><b><color=grey>
+ 
+ Automatic
+ disassembly</color></b></size></align>
+{{~set 'IDs' @root.E.Id}}
+{{~#each E.S.DockedE}}
+{{~set 'IDs' (concat @root.Data.IDs ',' Id)}}
+{{/each}}
+
+<size=30%>
+{{#scroll 24 1 4}}
+{{~#entitiesbyname '*'}}
+{{~#each .}}
+{{~#test Id in @root.Data.IDs}}{{else}}
+<color=white><u></color><color=red>{{Name}}</color></u> [<color=#{{~#test EntityType eq 'BA'}}F00{{/test}}{{~#test EntityType eq 'CV'}}F0F{{/test}}{{~#test EntityType eq 'SV'}}FA0{{/test}}{{~#test EntityType eq 'HV'}}00F{{/test}}>{{EntityType}}</color>]
+{{recycle . 'Box-Recycling-Foss*'}}  min: X:
+{{~MinPos.X}} Y:{{~MinPos.Y}} Z:{{~MinPos.Z}}
+  max: X:{{MaxPos.X}} Y:
+{{~MaxPos.Y}} Z:{{MaxPos.Z}}
+  current X:{{X}} Y:{{Y}} Z:{{Z}}
+  Blocks: Total:{{TotalBlocks}}  Checked:
+{{~CheckedBlocks}}  RemovedBlocks:
+{{~RemovedBlocks}}
+{{/recycle}}
+{{deconstruct . 'Box-Recycling-Foss*'}}  min: X:
+{{~MinPos.X}} Y:{{~MinPos.Y}} Z:{{~MinPos.Z}}
+  max: X:{{MaxPos.X}} Y:
+{{~MaxPos.Y}} Z:{{MaxPos.Z}}
+  current X:{{X}} Y:{{Y}} Z:{{Z}}
+  Blocks: Total:{{TotalBlocks}}  Checked:
+{{~CheckedBlocks}}  RemovedBlocks:
+{{~RemovedBlocks}}
+{{/deconstruct}}
+{{/test}}
+{{/each}}
+{{/entitiesbyname}}
+{{/scroll}}
+</size>
---
+++ 0. Stand Alone/Lists/Playfield Info.lua
@@ -0,0 +1,12 @@
+<size=-1><align=center><color=grey><color=white><u></color>{{P.PlayfieldType}}:</u></color></color></size>
+{{P.Name}} (<color={{~if P.IsPvP}}red>PvP{{else}}green>PvE{{/if}}</color>)
+ 
+<size=-1><color=grey><color=white><u></color>Type/Class:</u></color></color></size>
+{{P.PlanetType}}/{{P.PlanetClass}}
+ 
+<size=-1><color=grey><color=white><u></color>Bioscan Results:</u></color></color></size>
+{{~scroll 3 1}}
+{{~#each P.Players}}
+ - {{Name}}
+{{/each}}
+{{/scroll}}
