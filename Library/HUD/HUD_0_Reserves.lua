4a6cd49f2982d8577a6add63b1a27ad2409c8878
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-03-09 20:08:17 +0100

    Update HUD_0_Reserves.lua

--- Library/HUD/HUD_0_Reserves.lua
+++ Library/HUD/HUD_0_Reserves.lua
@@ -3,4 +3,5 @@ Reserves: <color=#{{@root.data.c1}}>{{format @root.Data.P '{0,0:0}'}}</color>% /
 <color=white>[<color=#{{@root.data.c1}}>{{bar @root.Data.P 0 100 15 '▰' '  ' }}</color>]</color>{{/devices}}
 <size=3><line-height=3>
 {{#items E.S. '*Reserve-*'}}
-☉ {{format count '{0,0:N0}'}}<pos=23>{{i18n key}}</pos>{{/items}}</size>{{/test}}
+☉ {{format count '{0,0:N0}'}}<pos=23>{{i18n key}}</pos>
+{{/items}}</size>{{/test}}
