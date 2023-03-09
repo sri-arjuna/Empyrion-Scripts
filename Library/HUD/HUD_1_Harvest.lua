a962bc816b8c5de689bee3adf674250cad1f80ce
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-03-09 17:29:22 +0100

    Updated HUD and Added Reserves Module

--- Library/HUD/HUD_1_Harvest.lua
+++ Library/HUD/HUD_1_Harvest.lua
@@ -1,5 +1,5 @@
 {{~#test (calc @root.CycleCounter '%' 7) eq 0}}{{~set 'Vol' 0}}{{~set 'Filled' 0}}{{~set 'P' 0}}{{~#devicesoftype E.S. 'HarvestCntr'}}{{~#each .}}{{set 'CN' CustomName}}{{~#math @root.data.Vol '+' ./Device.VolumeCapacity }}{{~set 'Vol' .}}{{~/math}}{{~#items @root.E.S @root.Data.CN}}{{~#math @root.Data.Filled '+' (calc (configattr Id 'Volume') '*' Count)}}{{~set 'Filled' .}}{{~/math}}{{~/items}}{{/each}}{{~#math (calc 100 '/' @root.Data.Vol ) '*' @root.data.Filled}}{{~set 'P' .}}{{/math}}{{~#test @root.Data.P leq '50'}}{{set 'c1' '1D1'}}{{/test}}{{~#test @root.Data.P in '50-75'}}{{set 'c1' 'DA1'}}{{/test}}{{~#test @root.Data.P in '75-90'}}{{set 'c1' 'B40'}}{{/test}}{{~#test @root.Data.P geq '90'}}{{set 'c1' 'D11'}}{{/test}}
 <line-height=4>
-Harvest: <color=#{{@root.data.c1}}>{{format @root.Data.P '{0,0:0}'}}</color>% / {{~#math @root.Data.Vol '/' 1000}}{{format . '{0,1:N0}'}}k{{/math}}
+Harvest: <color=#{{@root.data.c1}}>{{format @root.Data.P '{0,0:0}'}}</color>% / {{~#math @root.Data.Vol '/' 1000}}{{format . '{0,0:N1}'}}k{{/math}}
 <color=white>[<color=#{{@root.data.c1}}>{{bar @root.Data.P 0 100 15 '▰' ' ' }}</color>]</color>
 {{/devicesoftype}}{{/test}}
