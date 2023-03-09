5b043ecf9934efa8bc36e5417280423099ea2c4e
Arjuna S. Erat <sri-arjuna@users.noreply.github.com>
2023-03-09 12:32:30 +0100

    Update DSEV_HUD.lua
    Slight position fix, had the project previously rotate on x-axis (placed on floor), now optimized for 'wall placement'.

--- z_Build_Backups/1.9-REA-noMV/600i/HUD/DSEV_HUD.lua
+++ z_Build_Backups/1.9-REA-noMV/600i/HUD/DSEV_HUD.lua
@@ -1,4 +1,4 @@
-<line-height=-8>
+<line-height=-4>
 ╓────╌╌╌╌┄┄┄┄┈┈┈┈<line-height=5>
 ║
 ║
@@ -23,7 +23,7 @@
 <line-height=-39>
 <line-height=7>
 <align=center>-=❲<i><color=orange>{{E.Name}}</color></i>❳=-
-<line-height=174>
+<line-height=173>
 <line-height=10>
 <align=center><size=3><i>Dynamic HUD with Scripts, by DSEV - Deep Space Exploration Versatiles (Sephrajin), to hide/disable it, use P-Menu --> "Toggle Scripts"</i></size></align>
 <line-height=-46>
@@ -31,12 +31,12 @@
 Damage {{#use E.S.DamageLevel}}[<color={{test . leq 5}}green{{else}}red{{/test}}>{{bar . 0 100 40 '▰' ' ' }}</color>]{{~format . '{0,8:P2}'}}{{/use}}
 </align>
 <line-height=-125>
-<indent=285><line-height=8>
-<align=center><color=white><u></color><size=6>☤ <color=green><i>Bio Scan:</i></color> ☤</size></u><size=4>
+<indent=265><line-height=8>
+<align=center><color=white><u></color><size=6>☤ <color=green><i>Bio Life Scanner:</i></color> ☤</size></u><size=4>
 <color=white><line-height=6>{{~scroll 2 1 1}}{{#each P.Players}}[ <i><color=#1affff>{{Name}}</color></i> ]
 {{/each}}
 {{/scroll}}</color><line-height=8>
-<align=center><color=white><u></color><size=6>✈ <color=yellow><i>Docked:</i></color> ✈</size></u><size=4>
+<align=center><color=white><u></color><size=6>✈ <color=yellow><i>Docked Vessels:</i></color> ✈</size></u><size=4>
 <color=white><line-height=6>{{~scroll 2 1 1}}{{#each E.S.DockedE}}[ <i><color=#1affff>{{Name}}</color></i> ] {{~#if S.IsPowerd}}<color=red> !! ON !!</color>{{/if}}
 {{/each}}
 {{/scroll}}</color>
